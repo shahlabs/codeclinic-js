80e887476a54955834db4ca1232a3db2bd437a6b
Labdhi Shah <labdhi.shah@slalom.com>
2023-01-03 15:22:30 -0500

    update gulp to v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,37 @@
 const gulp = require('gulp'),
   webserver = require('gulp-server-io'),
   dest = 'builds/codeclinic/';
+    gulp.task('html', function() {
+      gulp.src(dest + '**/*.html');
+    });
+    
+    // Regular CSS
+    gulp.task('css', function() {
+      gulp.src(dest + '**/*.css');
+    });
+    
+    // JavaScript ES6
+    gulp.task('js', function() {
+      gulp.src(dest + '**/*.js');
+    });
+    
+    gulp.task('watch', function() {
+      gulp.watch(dest + '**/*.js', gulp.series('js'));
+      gulp.watch(dest + '**/*.css', gulp.series('css')); //CSS
+      gulp.watch(dest + '**/*.html', gulp.series('html'));
+    });
+    
+    gulp.task('webserver', function() {
+      gulp.src(dest).pipe(
+        webserver({
+          serverReload: {
+            dir: dest,
+            config: { verbose: true, debounce: 1000 }
+          },
+          port: 3000,
+          open: true
+        })
+      );
+    });
 
-gulp.task('html', function() {
-  gulp.src(dest + '**/*.html');
-});
-
-// Regular CSS
-gulp.task('css', function() {
-  gulp.src(dest + '**/*.css');
-});
-
-// JavaScript ES6
-gulp.task('js', function() {
-  gulp.src(dest + '**/*.js');
-});
-
-gulp.task('watch', function() {
-  gulp.watch(dest + '**/*.js', ['js']);
-  gulp.watch(dest + '**/*.css', ['css']); //CSS
-  gulp.watch(dest + '**/*.html', ['html']);
-});
-
-gulp.task('webserver', function() {
-  gulp.src(dest).pipe(
-    webserver({
-      serverReload: {
-        dir: dest,
-        config: { verbose: true, debounce: 1000 }
-      },
-      port: 3000,
-      open: true
-    })
-  );
-});
-
-gulp.task('default', ['html', 'css', 'js', 'webserver', 'watch']);
+gulp.task('default', gulp.parallel(['html', 'css', 'js', 'webserver', 'watch']));
